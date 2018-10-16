5ec1705376cd17e995382a3ea76ac06fb88495af
kbbushman <k.bushman@yahoo.com>
2018-10-16 11:45:06 -0700

    Adds sendmail config

--- btre/settings.py
+++ btre/settings.py
@@ -138,3 +138,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kennybdev@gmail.com'
+EMAIL_HOST_PASSWORD = 'U22bq+%H2NZWZ'
+EMAIL_USE_TLS = True
+
